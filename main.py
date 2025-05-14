17bf27d7db9f845dccdce73c2ec31b4799c49c92
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-05-14 18:07:14 +0300

    implement task put endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, status
-from models import Task, TaskCreate
+from models import Task, TaskCreate, TaskUpdate
 from typing import List
 
 app = FastAPI()
@@ -31,6 +31,27 @@ def create_task(task_input: TaskCreate):
     next_task_id += 1
 
     return new_task
+
+@app.put('/tasks/{task_id}', response_model=Task)
+def update_task(task_id: int, task_update_input:TaskUpdate):
+    task_to_update = next((task for task in tasks if task.id == task_id), None)
+
+    if task_to_update is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Task with ID {task_id} does not exist"
+        )
+
+    update_data = task_update_input.model_dump(exclude_unset=True)
+
+    if "name" in update_data:
+        task_to_update.name = update_data['name']
+    if "description" in update_data:
+        task_to_update.description = update_data['description']
+    if "is_completed" in update_data:
+        task_to_update.is_completed = update_data['is_completed']
+
+    return task_to_update
 
 @app.delete('/tasks/{task_id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_task(task_id: int):
